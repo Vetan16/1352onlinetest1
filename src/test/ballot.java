687b4be189cb366cdad27117c984b2bfd599fe98
Vetan16 <vetansalgaoncar@gmail.com>
2015-07-09 17:28:02 +0530

    version4

--- src/test/ballot.java
+++ src/test/ballot.java
@@ -3,20 +3,22 @@ package test ;
 public class ballot {
 	
 	private control control;
-	
+	private boolean setCandidate = false;
 
 	public void attachControlUnit( control control){
 		this.control = control;
 	}
 	
 
-	public void pressCandidateKeyForVote( Integer sno ){
-		control.putvote( sno );
+	public void pressCandidateKey( Integer SNO ){
+		if(setCandidate)
+			control.setTotalCandidates(SNO);
+		else
+			control.casteAVote( SrNo );
 	}
 	
-	public void allowVote(){
-		
+	public void setCandidate(){
+		this.setCandidate = true;
 	}
 }
-
 
--- src/test/control.java
+++ src/test/control.java
@@ -1,10 +1,26 @@
 /*Singleton design pattern*/
-
 package test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class control {
 	
+	private String constituency;
+	
+	private ArrayList<Integer> voteCounter;
+	
+	private VotePermissionState VAState = new block();
+	
+	private Integer totalCandidates;
+	
+	/* reference to the ballot unit */
+	private ballot ballotUnit;
+	
+	/* sole instance */
 	private static control instance;
+	
 	
 	public static control getInstance(){
 		if( instance == null )
@@ -13,14 +29,46 @@ public class control {
 		return instance;
 	}
 	
-	/*ready to voting*/
+	private control(){
+		this.voteCounter = new ArrayList<Integer>();
+	}
+	
+	
 	public void pressBallotButton(){
-		
+		this.VAState.switchState(this);
 	}
 	
-	/* connects cu to bu*/
-	public void connectBallotUnit(){
-		
+	
+	public void connectBallotUnit( ballot ballot ){
+		this.ballotUnit = ballotUnit;
 	}
 	
+	
+	public void detachBallotUnit(){
+		this.ballot = null;
+	}
+	
+	
+	public void casteAVote( Integer candidate ){
+		
+		if( this.VAState instanceof allow ){
+			this.voteCounter.set(candidate, this.voteCounter.get(candidate)+1);
+			this.switchVotePermission();
+		}
+		
+	}
+
+
+	public void setTotalCandidates( Integer total ){
+		this.totalCandidates = total;
+	}
+	
+	
+	public void switchVotePermission(){
+		this.VAState.switchState(this);
+	}
+	
+	public void setVotePermission( VotePermissionState vs ){
+		this.VAState = vs;
+	}
 }
